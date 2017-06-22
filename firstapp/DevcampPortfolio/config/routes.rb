253969c777bc7c1ca4a26855f7d426ff14403d9c
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 15:17:11 +0000

    added pages and custom routes

--- firstapp/DevcampPortfolio/config/routes.rb
+++ firstapp/DevcampPortfolio/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :portfolios
+  
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'about', to: 'pages#about'
+  
+  get 'contact', to: 'pages#contact'
+  
+  root to: 'pages#home'  
 end
