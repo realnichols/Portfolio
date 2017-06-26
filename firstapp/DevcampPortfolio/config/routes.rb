c7ee1c8a6ffdf12720c7e92a9fe0f300f12cfafc
Jordan Hudgens <jordan@devcamp.com>
2017-06-26 15:30:17 +0000

    status update

--- firstapp/DevcampPortfolio/config/routes.rb
+++ firstapp/DevcampPortfolio/config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
   
   get 'about', to: 'pages#about'
-  
   get 'contact', to: 'pages#contact'
   
-  root to: 'pages#home'  
+  root to: 'pages#home'
 end
